eebe1ff2005fb73563d7f0363273a92a462777d9
Xolgrunek <xol@xolgoth.xyz>
2022-10-05 09:41:59 -0500

    automatic redirect

--- newstreamer/add.php
+++ newstreamer/add.php
@@ -7,9 +7,12 @@ $sql = "INSERT INTO streamerPeople (username, streamlink) VALUES (%$username%, %
 
 if ($conn->query($sql) === TRUE) {
     echo "New Streamer added successfully!";
+    #echo "<a href=newstreamer.php>Add Another Streamer</a>";
 } else {
     echo "Error, contact Xol: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
+echo "sleep 10";
+header("Location: newstreamer.php"); 
 ?>
